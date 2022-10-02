6a32fc1ffc9aee21125f4df30002d7c09eed8421
Beatriz Gutiérrez Arazo <beagutara@alum.us.es>
2022-10-02 20:16:32 +0200

    Proyecto

--- src/test_calificaciones.py
+++ src/test_calificaciones.py
@@ -1,3 +1,7 @@
 from calificaciones import *
-notaAda = calcular_nota(27,23,3)
-print(notaAda)
+
+totalRespuestas = int(input("¿Cuántas preguntas tiene el test? "))
+aciertos = int(input("¿Cuántas ha acertado? "))
+errores = int(input("¿Cuántas ha fallado? "))
+notaAda = calcular_nota(totalRespuestas,aciertos,errores)
+print("Ada, quien ha acertado", aciertos, "y fallado", errores, "de las", totalRespuestas, "preguntas, ha sacado un", notaAda)
