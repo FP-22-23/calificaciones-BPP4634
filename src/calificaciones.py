1d8166cca518fac2bf6b3e81f4bfa35623b4e565
Beatriz Gutiérrez Arazo <beagutara@alum.us.es>
2022-10-02 20:43:44 +0200

    Proyecto

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -3,3 +3,17 @@ def calcular_nota(totalRespuestas,aciertos,errores):
     if nota < 0:
         nota = 0
     return nota
+
+def calcula_nota_cuatrimestre(cuestionarios, parcial, proyecto):
+    if proyecto<5:
+        return 3
+    else:
+        return 0.1*sum(cuestionarios)+0.6*parcial+0.1*proyecto
+
+def calcula_nota_evaluacion_continua(cuestionarios, parciales, proyecto):
+    cuatri1 = calcula_nota_cuatrimestre((cuestionarios[0], cuestionarios[1], cuestionarios[2]), parciales[0], proyecto[0])
+    cuatri2 = calcula_nota_cuatrimestre((cuestionarios[3], cuestionarios[4], cuestionarios[5]), parciales[1], proyecto[1])
+    if cuatri1<4 or cuatri2<4:
+        return 0
+    else:
+        return (cuatri1+cuatri2)/2
--- src/test_calificaciones.py
+++ src/test_calificaciones.py
@@ -1,7 +1,10 @@
 from calificaciones import *
-
+'''
 totalRespuestas = int(input("¿Cuántas preguntas tiene el test? "))
 aciertos = int(input("¿Cuántas ha acertado? "))
 errores = int(input("¿Cuántas ha fallado? "))
 notaAda = calcular_nota(totalRespuestas,aciertos,errores)
 print("Ada, quien ha acertado", aciertos, "y fallado", errores, "de las", totalRespuestas, "preguntas, ha sacado un", notaAda)
+'''
+print(calcula_nota_cuatrimestre((7, 8 ,7), 10, 6))
+print(calcula_nota_evaluacion_continua((7, 8 ,7, 6, 9, 10), (10, 9), (6, 7)))
